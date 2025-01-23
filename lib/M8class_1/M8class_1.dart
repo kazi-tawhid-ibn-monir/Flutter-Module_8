ea528904219d7a730ff542705c0477925c4214a2
KaziTanvir <kazitawhidibnmonir@gmail.com>
2025-01-23 12:23:04 +0600

    Module-8 class-1 source code and practice code

---
+++ lib/M8class_1/M8class_1.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple,
+          title: Center(
+            child: Text(
+              "Flutter App Development",
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+          ),
+        ),
+        body: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
